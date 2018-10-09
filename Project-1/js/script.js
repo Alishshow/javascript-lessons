f016623ed92d96a90999f81f7e6ddf46b023d8f6
Alishshow <alishshow581@gmail.com>
2018-10-10 02:26:39 +0600

    Переделал

--- Project-1/js/script.js
+++ Project-1/js/script.js
@@ -1,19 +1,28 @@
-var money = +prompt("Ваш бюджет на месяц?");
-var time = prompt("Введите дату в формате YYYY-MM-DD");
-var appData = {
+'use strict';
+
+let money = +prompt("Ваш бюджет на месяц?", "60000");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "2018-10-10");
+let appData = {
     budget: money,
     timeData: time,
     expenses: {},
     optionalExpenses: {},
     income: [],
     savings: false
-}
-var answer = prompt("Введите обязательную статью расходов в этом месяце"),
-    answer2 = prompt("Во сколько обойдется?");
-    answer = prompt("Введите обязательную статью расходов в этом месяце");
-    answer2 = prompt("Во сколько обойдется?");
-    expenses = {
-        1 : answer,
-        2 : answer2
-    }
+};
+let answer = prompt("Введите обязательную статью расходов в этом месяце", "Еда"),
+    answer2 = prompt("Во сколько обойдется?", "20000");
+    answer = prompt("Введите обязательную статью расходов в этом месяце", "Еда"),
+    answer2 = prompt("Во сколько обойдется?", "200000");
+let expenses = {
+        1: answer,
+        2: answer2
+    };
+    console.log(typeof(answer));
 alert(money / 30);
+console.log(money);
+console.log(money/30);
+console.log(answer);
+console.log(answer2);
+console.log(expenses);
+
